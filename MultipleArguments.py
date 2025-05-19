12f763312ebb6b6d8299b297248987f6989a901c
mercy-5120 <mgashau18@gmail.com>
2025-05-19 22:01:23 +0300

    How to write multiple arguments

---
+++ MultipleArguments.py
@@ -0,0 +1,10 @@
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+
+def add_numbers(n1, n2):
+    result = n1 + n2
+    return result
+
+
+result = add_numbers(n1, n2)
+print("The sum is: ", result)
