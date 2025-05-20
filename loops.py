73eb0af04aaabb1e5ac2fd36a13fac6694794464
mercy-5120 <mgashau18@gmail.com>
2025-05-20 09:30:27 +0300

    Loops in python

---
+++ loops.py
@@ -0,0 +1,14 @@
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+numbers = [1, 2, 3, 4, 5]
+for item in numbers:
+    print(item)
+
+
+i = 0
+while i< len(numbers):
+    print(numbers[i])
+    i += 1
