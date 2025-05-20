d59e108711526f5d106d1cf5f907488c93b06d85
mercy-5120 <mgashau18@gmail.com>
2025-05-20 09:26:54 +0300

    Lists in python

---
+++ lists.py
@@ -0,0 +1,26 @@
+names = ["Mercy", "Steph", "Joseph", "David"]
+print(names[-4])
+print(names[1])
+
+names[0]= "Meecy"
+print(names[0])
+
+print (names[0:2])
+
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+
+numbers.insert(0, -1)
+print(numbers)
+
+numbers.remove(1)
+print(numbers)
+
+#numbers.clear()
+#print(numbers)
+
+
+print(2 in numbers)
+
+print(len(numbers))
